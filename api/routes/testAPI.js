5dd78c6f2c84f500bfdd9d6d2f2c8841a51f8df9
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-18 20:06:13 +0300

    Puppeteer get multiple elements being tested.

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -5,31 +5,45 @@ var router = express.Router();
 
 const pt= require('puppeteer');
 
-async function getText(){
-   //launch browser in headless mode
-   const browser = await pt.launch()
-   //browser new page
-   const page = await browser.newPage()
-   //launch URL
-   await page.goto('https://www.tutorialspoint.com/about/about_careers.htm')
-   //identify element
-   const f = await page.$("[class='heading']")
-   //obtain text
-   const text = await (await f.getProperty('textContent')).jsonValue()
+async function run(){
+    const browser = await pt.launch()
+    const page = await browser.newPage()
+    await page.goto('https://www.youtube.com/watch?v=7aPzchOkxXk')
 
-   if(text)
-   {
-            router.get("/", function(req, res, next) {
-            res.json({
-                t: text
-            });
-            });
-   }
-   
-   
+    const elements = await page.$$("div");
+    elements.forEach(async element => {
+	const text = await (await element.getProperty("textContent")).jsonValue();
+	console.log(text);
+});
 }
+run()
 
-getText();
+
+// async function getText(){
+//    //launch browser in headless mode
+//    const browser = await pt.launch()
+//    //browser new page
+//    const page = await browser.newPage()
+//    //launch URL
+//    await page.goto('https://www.tutorialspoint.com/about/about_careers.htm')
+//    //identify element
+//    const f = await page.$("[class='heading']")
+//    //obtain text
+//    const text = await (await f.getProperty('textContent')).jsonValue()
+
+//    if(text)
+//    {
+//             router.get("/", function(req, res, next) {
+//             res.json({
+//                 t: text
+//             });
+//             });
+//    }
+   
+   
+// }
+
+// getText();
 
 
 // router.get("/", function(req, res, next) {
