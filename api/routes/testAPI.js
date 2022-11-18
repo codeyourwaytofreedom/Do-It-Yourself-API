ccab98ef33cc5199f2c9d24e521188975bb4b55a
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-18 17:13:42 +0300

    Puppeteer worked. Scraped text sent via API. Text receieved from API displayed in React App. More elaborate scraping to be tried. Great step forward.
    Keep going.
    
    Remember why you started.

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -2,12 +2,37 @@
 var express = require("express");
 var router = express.Router();
 
-var c = 5
-var d = 7
 
-router.get("/", function(req, res, next) {
-    res.send((c*d).toString());
-});
+const pt= require('puppeteer');
+
+async function getText(){
+   //launch browser in headless mode
+   const browser = await pt.launch()
+   //browser new page
+   const page = await browser.newPage()
+   //launch URL
+   await page.goto('https://www.tutorialspoint.com/about/about_careers.htm')
+   //identify element
+   const f = await page.$("[class='heading']")
+   //obtain text
+   const text = await (await f.getProperty('textContent')).jsonValue()
+
+   if(text)
+   {
+            router.get("/", function(req, res, next) {
+            res.send(text);
+            });
+   }
+   
+   
+}
+
+getText();
+
+
+// router.get("/", function(req, res, next) {
+//     res.send("Hello world");
+// });
 
 module.exports = router;
 
