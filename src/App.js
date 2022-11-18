7440dc96764caad84d92e16b3f76fbb2a4ab996e
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-18 15:41:31 +0300

    Express-created API is working. React app is receiving API response and.. displaying it in html.
    Front-end & Back-end connection done.
    
    A very important step for the new project.
    
    A lot to be done with API and scraping.
    
    Keep going.
    
    Remember why you started.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [Data, setData] = useState(null);
+
+  useEffect(()=> {
+
+      axios.get("http://localhost:9000/testAPI").then((response) => {
+        setData(response.data);
+      });
+    
+  },[])
+
   return (
     <div className="App">
-          <h1>DIY API</h1>
+          <h1>{Data ? Data : 5}</h1>
     </div>
   );
 }
