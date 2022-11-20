9debaa4ce95cb9b136713e12a8c5eee88338c26c
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-20 12:19:39 +0300

    polify problem solved. Webconfig file updated.
    Repetitive words in comments sorted.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import axios from "axios";
 
 function App() {
   const [Data, setData] = useState(null);
+  const [sorted, setSorted] = useState(null);
 
   useEffect(()=> {
 
       axios.get("http://localhost:9000/testAPI").then((response) => {
         setData(response.data);
-        for (let index = 0; index < Data.length; index++) {
-          const element = Data[index].comment.split(" ");
-          console.log(element)
+        let all_words = [];
+        let sorted = [];
+        if(Data){
+          for (let index = 0; index < Data.length; index++) {
+            const element = Data[index].comment.split(" ");
+            for (let index = 0; index < element.length; index++) {
+              const word = element[index];
+              all_words.push(word);
+              if(!sorted.includes(word))
+              {sorted.push(word)}
+            }
+          }
+          setSorted(sorted.length)
+
+          console.log(sorted.sort())
         }
+          
+          
+
+        
+        
       });
     
   },[])
@@ -28,8 +46,8 @@ function App() {
           {
               Data && Data.map((comment, index)=>
               <div key={index} className="comment">
-                {comment.comment.split(" ").map((word)=>
-                  <span>{word}</span>
+                {comment.comment.split(" ").map((word, index)=>
+                  <span key={index}>{word}</span>
                 )
                 }
               </div>
