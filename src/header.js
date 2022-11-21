1faa924e3655d6f41037f360307fd115b0655e98
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-21 19:32:55 +0300

    Gradual display of footprints under construction. State used for this.

--- src/header.js
+++ src/header.js
@@ -2,8 +2,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import  {} from '@fortawesome/free-solid-svg-icons';
 
 import "./header.css";
+import { useRef, useState, useEffect } from "react";
 
 const Header = () => {
+
+const left = useRef();
+const [visible, setVisible] = useState(false)
+
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if(visible){setVisible(false)}
+            else{setVisible(true)}
+        }, 2000);
+      
+        return () => clearInterval(intervalId);
+      }, );
+
+
     return ( 
         <div className="header">
             <div className="youtube_logo">
@@ -11,15 +27,14 @@ const Header = () => {
             </div>
             <h1>YouTube Analytics</h1>
             <div className="foots">
-                <div className="left-foot">
+                <div className="left-foot" style={{opacity:visible ? "1" : 0 }}>
                     <img src={require("./images/left-footprint.png")} alt="a" />
                 </div> 
-                <div className="right-foot">
+                <div className="right-foot" style={{opacity:visible ? "1" : 0, transitionDelay:"0.2s" }}>
+                    
                     <img src={require("./images/right-footprint.png")} alt="a" />
                 </div> 
             </div>
-            
-            
         </div>
      );
 }
