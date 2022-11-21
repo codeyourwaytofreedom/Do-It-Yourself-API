aa1bb040031b59758829a363e220d9bf74ee8241
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-21 20:26:59 +0300

    walking footprints animation achieved. Great step for the design. might elaborate it later on.
    Keep going.
    
    Remember why you started.

--- src/header.js
+++ src/header.js
@@ -7,17 +7,17 @@ import { useRef, useState, useEffect } from "react";
 const Header = () => {
 
 const left = useRef();
-const [visible, setVisible] = useState(false)
+const [visible, setVisible] = useState(true)
 
 
-    useEffect(() => {
+    /* useEffect(() => {
         const intervalId = setInterval(() => {
             if(visible){setVisible(false)}
             else{setVisible(true)}
-        }, 2000);
+        }, 1000);
       
         return () => clearInterval(intervalId);
-      }, );
+      }, ); */
 
 
     return ( 
@@ -27,13 +27,44 @@ const [visible, setVisible] = useState(false)
             </div>
             <h1>YouTube Analytics</h1>
             <div className="foots">
-                <div className="left-foot" style={{opacity:visible ? "1" : 0 }}>
-                    <img src={require("./images/left-footprint.png")} alt="a" />
-                </div> 
-                <div className="right-foot" style={{opacity:visible ? "1" : 0, transitionDelay:"0.2s" }}>
-                    
-                    <img src={require("./images/right-footprint.png")} alt="a" />
-                </div> 
+                <div className="foots_left">
+                    <div className="foot" style={{opacity:visible ? "1" : 0 }}>
+                        <img src={require("./images/left-footprint.png")} alt="a" />
+                    </div> 
+                    <div className="foot" style={{opacity:visible ? "1" : 0 }}>
+                    </div> 
+                    <div className="foot" style={{opacity:visible ? "1" : 0,  transitionDelay:visible ? "0.4s" : "0s"  }}>
+                        <img src={require("./images/left-footprint.png")} alt="a" />
+                    </div> 
+                    <div className="foot" style={{opacity:visible ? "1" : 0 }}>
+                    </div> 
+                    <div className="foot" style={{opacity:visible ? "1" : 0, transitionDelay:visible ? "0.8s" : "0s"  }}>
+                        <img src={require("./images/left-footprint.png")} alt="a" />
+                    </div> 
+                    <div className="foot" style={{opacity:visible ? "1" : 0 }}>
+                    </div> 
+                </div>
+                <div className="foots_right">
+                    <div className="foot" style={{opacity:visible ? "1" : 0, transitionDelay:visible ? "0.2s" : "0s" }}>
+                    </div> 
+                    <div className="foot" style={{opacity:visible ? "1" : 0, transitionDelay:visible ? "0.2s" : "0s" }}>
+                        <img src={require("./images/right-footprint.png")} alt="a" />
+                    </div> 
+                    <div className="foot" style={{opacity:visible ? "1" : 0, transitionDelay:visible ? "0.2s" : "0s" }}>
+                    </div> 
+                    <div className="foot" style={{opacity:visible ? "1" : 0, transitionDelay:visible ? "0.6s" : "0s" }}>
+                        <img src={require("./images/right-footprint.png")} alt="a" />
+                    </div> 
+                    <div className="foot" style={{opacity:visible ? "1" : 0, transitionDelay:visible ? "0.2s" : "0s" }}>
+                    </div> 
+                    <div className="foot" style={{opacity:visible ? "1" : 0, transitionDelay:visible ? "1.0s" : "0s" }}>
+                        <img src={require("./images/right-footprint.png")} alt="a" />
+                    </div> 
+                </div>
+
+                
+                
+                
             </div>
         </div>
      );
